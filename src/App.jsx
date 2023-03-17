7cc3a3c1170413bb06f5ff845d24c360ce2d5b71
MaltaWebDev <MaltaWebDev@gmail.com>
2023-03-17 19:40:18 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
-import React from "react";
+import Header from "./components/Header";
 
 function App() {
-  const title = "Blog Post";
-  const body = "This is my first blog post.";
-  const comments = [
-    { id: 1, text: "this is the 1st comment" },
-    { id: 2, text: "this is the 2nd comment" },
-    { id: 3, text: "this is the 3rd comment" },
-    { id: 4, text: "this is the 4th comment" },
-  ];
-
   return (
-    <div className="container">
-      <h1>{title.toUpperCase()}</h1>
-      <p> {body} </p>
-      {Math.random() * 100}
-    </div>
+    <>
+      <Header />
+      <div>
+        <h1>My App</h1>;
+      </div>
+    </>
   );
 }
 
